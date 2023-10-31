94dd0f1653c7632a599bd19db1a8ed0a8d41a836
Jose <126101249+JoseGeek78@users.noreply.github.com>
2023-10-31 16:27:43 +0000

    Update

--- src/js/index.js
+++ src/js/index.js
@@ -2,72 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
-import SecondsCounter from "./component/SecondsCounter";
+import App from "./component/App.jsx";
 
-
-let hoursDos = 0;
-let hours = 0;
-let minutesDos = 0;
-let minutes = 0;
-let seconds = 0;
-let miliSeconds = 0;
-
-setInterval(() => {
-
-    if (miliSeconds == 9) {
-        miliSeconds = 0;
-        seconds++;
-    }
-
-    if (seconds == 6) {
-        miliSeconds = 0;
-        seconds = 0;
-        minutes++;
-    }
-
-    if (minutes == 9) {
-        miliSeconds = 0;
-        seconds = 0;
-        minutes= 0;
-        minutesDos++;
-    }
-
-    if (minutesDos == 6) {
-        miliSeconds = 0;
-        seconds = 0;
-        minutes= 0;
-        minutesDos= 0;
-        hours++;
-    }
-
-    if (hours == 9) {
-        miliSeconds = 0;
-        seconds = 0;
-        minutes= 0;
-        minutesDos= 0;
-        hours= 0;
-        hoursDos++;
-    }
-
-    if (hoursDos == 2 && hours == 4) {
-        miliSeconds = 0;
-        seconds = 0;
-        minutes= 0;
-        minutesDos= 0;
-        hours= 0;
-        hoursDos= 0;
-    }
-
-
-
-
-    ReactDOM.render(
-        <SecondsCounter miliSeconds={miliSeconds} seconds={seconds} minutes={minutes} minutesDos={minutesDos} hours={hours} hoursDos={hoursDos}/>,
-        document.querySelector("#app")
-    );
-
-    miliSeconds++;
-
-}, 1000);
+ReactDOM.render(<App />, document.getElementById("app"));
